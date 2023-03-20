f23f5940404793c14188c1799aec61dfa6ce6a47
sakshi-si <97786311+sakshi-si@users.noreply.github.com>
2023-03-20 13:25:20 +0530

    ghjkl

--- src/App.js
+++ src/App.js
@@ -1,67 +1,62 @@
-import Navbar from './components/Navbar'
-import TextForm from './components/TextForm'
-import './App.css'
-import { useState } from 'react'
-import Alert from './components/Alert'
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 import About from './components/About';
-import { BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
+ 
 function App() {
-  const [mode, setMode] = useState('light') //whether dark mode is enabled or not
-  const [alert, setAlert] = useState(null)
-  const showAlert = (message, type) => {
-    setAlert({
-      msg: message,
-      type: type
-    })
-    setTimeout(() => {
-        setAlert(null)
-      },
-      1500)
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
   }
-  const toggleMode = () => {
-    if (mode === 'light') {
-      setMode('dark')
-      document.body.style.backgroundColor = '#042743'
-      showAlert('Dark mode has been enabled', 'success')
-      document.title='TextUtils - Dark Mode'
-    } else {
-      setMode('light')
-      document.body.style.backgroundColor = 'white'
-      showAlert('light mode has been enabled', 'success')
-      document.title='TextUtils - light Mode'
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
     }
   }
-
   return (
     <>
     <Router>
-      <Navbar
-        title="TextUtils"
-        aboutText="About"
-        mode={mode}
-        toggleMode={toggleMode}
-      />{' '}
-      <Alert alert={alert} />
-      <div className="container my-3">
-      <Switch>
-     <Route exact path='/about'>
-     <About mode={mode}/>
-    </Route>
-   
-    <Route exact path="/">
-    <TextForm
-          showAlert={showAlert}
-          heading="Enter the text to analyze below"
-          mode={mode}
-        />
- </Route>
- </Switch>
-      </div>
-
-     </Router> 
-         </>
-  )
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    <Switch>
+    {/* /users --> Component 1
+        /users/home --> Component 2 */}
+          <Route exact path="/about">
+            <About mode={mode} />
+          </Route>
+          <Route exact path="/">
+            <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
+          </Route>
+    </Switch>
+    </div>
+    </Router>
+    </> 
+  );
 }
 
-export default App
+export default App;
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,138 +1,65 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
+
 
 export default function TextForm(props) {
-  const handleUpClick = () => {
-    // console.log("Uppercase was clicked" + text)
-    let newText = text.toUpperCase()
-    setText(newText)
-    props.showAlert('Converted to Uppercase', 'success')
-  }
-  const handleClClick = () => {
-    // console.log("Uppercase was clicked" + text)
-    let newText = ' '
-    setText(newText)
-    props.showAlert('Text cleared!', 'success')
-  }
-  const handleLoClick = () => {
-    // console.log("Uppercase was clicked" + text)
-    let newText = text.toLowerCase()
-    setText(newText)
-    props.showAlert('Converted to lowercase!', 'success')
-  }
-  const speak = () => {
-    let msg = new SpeechSynthesisUtterance(text)
-    window.speechSynthesis.speak(msg)
-    const toggle = document.getElementById('toggle')
-
-    if (toggle.textContent === 'Speak') {
-      props.showAlert('Speaking written text!', 'success')
-      toggle.innerHTML = 'Stop'
-    } else {
-      toggle.innerHTML = 'Speak'
-      if ((toggle.innerHTML = 'Speak')) {
-        window.speechSynthesis.cancel()
-      }
+    const handleUpClick = ()=>{
+        let newText = text.toUpperCase();
+        setText(newText)
+        props.showAlert("Converted to uppercase!", "success");
     }
-  }
-  const handleCopy = () => {
-    console.log('I am copy')
-    var text = document.getElementById('mybox')
-    text.select()
-    text.setSelectionRange(0, 9999)
-    navigator.clipboard.writeText(text.value)
-    props.showAlert('Copied to Clipboard!', 'success')
-  }
-  const handleExtraSpaces = () => {
-    let newText = text.split(/[ ]+/)
-    setText(newText.join(' '))
-    props.showAlert('Extra spaces removed!', 'success')
-  }
 
-  const handleOnchange = (event) => {
-    // console.log("On change")
-    setText(event.target.value)
-  }
-  const [text, setText] = useState('')
-  return (
-    <>
-      <div style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
-        <h1> {props.heading} </h1>{' '}
-        <div class="mb-3">
-          <textarea
-            className="form-control"
-            value={text}
-            onChange={handleOnchange}
-            style={{
-              backgroundColor: props.mode === 'dark' ? '#13466e' : 'white',
-              color: props.mode === 'dark' ? 'white' : 'black',
-            }}
-            id="mybox"
-            rows="8"
-          >
-          
-          </textarea>{' '}
-        </div>{' '}
-        <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>
-          {' '}
-          Convert to Uppercase{' '}
-        </button>{' '}
-        <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>
-          {' '}
-          Convert to Lowercase{' '}
-        </button>{' '}
-        <button className="btn btn-primary mx-1 my-1" onClick={handleClClick}>
-          {' '}
-          Clear Text{' '}
-        </button>{' '}
-        <buttton
-          type="submit"
-          onClick={speak}
-          className="btn btn-primary mx-2 my-2"
-          id="toggle"
-        >
-          {' '}
-          Speak{' '}
-        </buttton>{' '}
-        <button className="btn btn-primary mx-1 my-1" onClick={handleCopy}>
-          {' '}
-          Copy Text{' '}
-        </button>{' '}
-        <button
-          className="btn btn-primary mx-1 my-1"
-          onClick={handleExtraSpaces}
-        >
-          {' '}
-          Remove extra spaces{' '}
-        </button>{' '}
-      </div>{' '}
-      <div
-        className="container my-3"
-        style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}
-      >
-        <h2> Your text summary </h2>{' '}
-        <p>
-          {' '}
-          {
-            text.split(' ').filter((element) => {
-              return element.length !== 0
-            }).length
-          }
-          words and {text.length}
-          characters
-        </p>
-        <p>
-          
-          {0.008 * text.split(' ').length.filter((element) => {
-              return element.length !== 0}.length}
-          Minutes read
-        </p>{' '}
-        <h2> Preview </h2>{' '}
-        <p>
-          {text.length > 0
-            ? text
-            : 'Enter something in the textbox above to preview it here'}{' '}
-        </p>{' '}
-      </div>{' '}
-    </>
-  )
+    const handleLoClick = ()=>{ 
+        let newText = text.toLowerCase();
+        setText(newText)
+        props.showAlert("Converted to lowercase!", "success");
+    }
+
+    const handleClearClick = ()=>{ 
+        let newText = '';
+        setText(newText);
+        props.showAlert("Text Cleared!", "success");
+    }
+
+    const handleOnChange = (event)=>{
+        setText(event.target.value) 
+    }
+
+    // Credits: A
+    const handleCopy = () => {
+        navigator.clipboard.writeText(text); 
+        props.showAlert("Copied to Clipboard!", "success");
+    }
+
+    // Credits: Coding Wala
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert("Extra spaces removed!", "success");
+    }
+
+    const [text, setText] = useState(''); 
+    // text = "new text"; // Wrong way to change the state
+    // setText("new text"); // Correct way to change the state
+    return (
+        <>
+        <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}> 
+            <h1 className='mb-4'>{props.heading}</h1>
+            <div className="mb-3"> 
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
+            </div>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+        </div>
+        <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
+            <h2>Your text summary</h2>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+            <p>{0.008 *  text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text.length>0?text:"Nothing to preview!"}</p>
+        </div>
+        </>
+    )
 }
